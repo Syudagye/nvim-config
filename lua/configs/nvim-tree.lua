202f429c39bb8043daa1ed4a9b0cc4e256eb130d
Syudagye <syudagye@gmail.com>
2023-05-25 12:54:46 +0200

    `project.nvim` integration with `nvim-tree`

--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -94,6 +94,13 @@ require 'nvim-tree'.setup {
     ignore = false,
     timeout = 400,
   },
+  -- Enable project.nvim integration
+  sync_root_with_cwd = true,
+  respect_buf_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_root = true
+  },
 }
 -- Yanked from NvChad
 vim.g.nvim_tree_icons = {
